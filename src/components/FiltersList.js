5b42a046693f79e246f6a47facb5a9ebf0585013
Ivan <ivan3123708@gmail.com>
2018-01-29 06:01:47 +0100

    Added filter options

--- src/components/FiltersList.js
+++ src/components/FiltersList.js
@@ -1,7 +1,7 @@
 import React from 'react';
-import { List, ListItem, ListItemText, ListSubheader, Collapse, Subheader } from 'material-ui';
-import ExpandLess from 'material-ui-icons/ExpandLess';
-import ExpandMore from 'material-ui-icons/ExpandMore';
+import { List, ListItem } from 'material-ui/List';
+import Subheader from 'material-ui/Subheader';
+import Checkbox from 'material-ui/Checkbox';
 import '../styles/FiltersList.css';
 
 class FiltersList extends React.Component {
@@ -14,22 +14,128 @@ class FiltersList extends React.Component {
     this.setState({ open: !this.state.open });
   };
 
+  handleNestedListToggle = (item) => {
+    this.setState({ open: item.state.open });
+  };
+
   render() {
     return (
       <div className="filtersList">
         <List>
-          <ListItem onClick={this.handleClick}>
-            {this.state.open ? <ExpandLess /> : <ExpandMore />}
-          </ListItem>
-          <ListItem primaryText="Brand"></ListItem>
-          <ListItem primaryText="Color"></ListItem>
-          <ListItem primaryText="OS"></ListItem>
-          <ListItem primaryText="Internal memory"></ListItem>
-          <ListItem primaryText="RAM"></ListItem>
-          <ListItem primaryText="Display size"></ListItem>
-          <ListItem primaryText="Display resolution"></ListItem>
-          <ListItem primaryText="Camera"></ListItem>
-          <ListItem primaryText="CPU"></ListItem>
+          <Subheader style={{ fontSize: '16px', fontWeight: 'bold' }}>Search by:</Subheader>
+          <ListItem 
+            primaryText="Price"
+            open={this.state.open}
+            onClick={this.handleClick}
+            onNestedListToggle={this.handleNestedListToggle}
+            nestedItems={[
+              <ListItem key={1} primaryText="< $100" leftCheckbox={<Checkbox />} />,
+              <ListItem key={1} primaryText="$100 - $250" leftCheckbox={<Checkbox />} />,
+              <ListItem key={1} primaryText="$250 - $500" leftCheckbox={<Checkbox />} />,
+              <ListItem key={1} primaryText="> $500" leftCheckbox={<Checkbox />} />
+            ]}
+          />
+          <ListItem 
+            primaryText="Brand"
+            open={this.state.open}
+            onClick={this.handleClick}
+            onNestedListToggle={this.handleNestedListToggle}
+            nestedItems={[
+              <ListItem key={1} primaryText="Samsung" leftCheckbox={<Checkbox />} />,
+              <ListItem key={1} primaryText="Apple" leftCheckbox={<Checkbox />} />,
+              <ListItem key={1} primaryText="Huawei" leftCheckbox={<Checkbox />} />,
+              <ListItem key={1} primaryText="LG" leftCheckbox={<Checkbox />} />,
+              <ListItem key={1} primaryText="HTC" leftCheckbox={<Checkbox />} />,
+            ]}
+          />
+          <ListItem 
+            primaryText="Color"
+            open={this.state.open}
+            onClick={this.handleClick}
+            onNestedListToggle={this.handleNestedListToggle}
+            nestedItems={[
+              <ListItem key={1} primaryText="Black" leftCheckbox={<Checkbox />} />,
+              <ListItem key={1} primaryText="White" leftCheckbox={<Checkbox />} />,
+              <ListItem key={1} primaryText="Grey" leftCheckbox={<Checkbox />} />
+            ]}
+          />
+          <ListItem 
+            primaryText="OS"
+            open={this.state.open}
+            onClick={this.handleClick}
+            onNestedListToggle={this.handleNestedListToggle}
+            nestedItems={[
+              <ListItem key={1} primaryText="Android" leftCheckbox={<Checkbox />} />,
+              <ListItem key={1} primaryText="iOS" leftCheckbox={<Checkbox />} />
+            ]}
+          />
+          <ListItem 
+            primaryText="Internal memory"
+            open={this.state.open}
+            onClick={this.handleClick}
+            onNestedListToggle={this.handleNestedListToggle}
+            nestedItems={[
+              <ListItem key={1} primaryText="8GB" leftCheckbox={<Checkbox />} />,
+              <ListItem key={1} primaryText="16GB" leftCheckbox={<Checkbox />} />,
+              <ListItem key={1} primaryText="32GB" leftCheckbox={<Checkbox />} />
+            ]}
+          />
+          <ListItem 
+            primaryText="RAM"
+            open={this.state.open}
+            onClick={this.handleClick}
+            onNestedListToggle={this.handleNestedListToggle}
+            nestedItems={[
+              <ListItem key={1} primaryText="1GB" leftCheckbox={<Checkbox />} />,
+              <ListItem key={1} primaryText="2GB" leftCheckbox={<Checkbox />} />,
+              <ListItem key={1} primaryText="3GB" leftCheckbox={<Checkbox />} />,
+              <ListItem key={1} primaryText="4GB" leftCheckbox={<Checkbox />} />
+            ]}
+          />
+          <ListItem 
+            primaryText="Display size"
+            open={this.state.open}
+            onClick={this.handleClick}
+            onNestedListToggle={this.handleNestedListToggle}
+            nestedItems={[
+              <ListItem key={1} primaryText="5''" leftCheckbox={<Checkbox />} />,
+              <ListItem key={1} primaryText="5.2''" leftCheckbox={<Checkbox />} />,
+              <ListItem key={1} primaryText="5.5''" leftCheckbox={<Checkbox />} />
+            ]}
+          />
+          <ListItem 
+            primaryText="Display resolution"
+            open={this.state.open}
+            onClick={this.handleClick}
+            onNestedListToggle={this.handleNestedListToggle}
+            nestedItems={[
+              <ListItem key={1} primaryText="1280x720" leftCheckbox={<Checkbox />} />,
+              <ListItem key={1} primaryText="1920x1080" leftCheckbox={<Checkbox />} />,
+              <ListItem key={1} primaryText="2560x1440" leftCheckbox={<Checkbox />} />
+            ]}
+          />
+          <ListItem 
+            primaryText="Camera"
+            open={this.state.open}
+            onClick={this.handleClick}
+            onNestedListToggle={this.handleNestedListToggle}
+            nestedItems={[
+              <ListItem key={1} primaryText="8Mpix" leftCheckbox={<Checkbox />} />,
+              <ListItem key={1} primaryText="12Mpix" leftCheckbox={<Checkbox />} />,
+              <ListItem key={1} primaryText="13Mpix" leftCheckbox={<Checkbox />} />
+            ]}
+          />
+          <ListItem 
+            primaryText="CPU"
+            open={this.state.open}
+            onClick={this.handleClick}
+            onNestedListToggle={this.handleNestedListToggle}
+            nestedItems={[
+              <ListItem key={1} primaryText="Quad Core" leftCheckbox={<Checkbox />} />,
+              <ListItem key={1} primaryText="Hexa Core" leftCheckbox={<Checkbox />} />,
+              <ListItem key={1} primaryText="Octa Core" leftCheckbox={<Checkbox />} />
+            ]}
+          />
         </List>
       </div>
     )
